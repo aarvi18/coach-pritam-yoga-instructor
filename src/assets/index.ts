1ebefccb6e39e9f46520849eb11bc5e03749ee71
Rakesh <rakeshcse123@gmail.com>
2025-04-07 19:34:26 +0530

    feat : added logo

---
+++ src/assets/index.ts
@@ -0,0 +1,1 @@
+export {default as Logo} from './coach-pritam-logo.png'
--- src/components/navbar/NavbarLogo.tsx
+++ src/components/navbar/NavbarLogo.tsx
@@ -1,7 +1,17 @@
 // NavbarLogo.tsx
-import React from 'react'
-import { main } from '../../data/main'
+import React from 'react';
+import { main } from '../../data/main';
+import { Logo } from '../../assets'; // adjust the path according to your project structure
 
 export const NavbarLogo: React.FC = () => (
-  <div className="text-2xl font-bold text-primary">{main.companyName}</div>
-)
+  <div className="flex items-center space-x-2">
+    <img
+      src={Logo}
+      alt="Logo"
+      className="h-8 w-8 sm:h-12 sm:w-12 object-contain"
+    />
+    <span className="text-md sm:text-2xl font-bold text-primary">
+      {main.companyName}
+    </span>
+  </div>
+);
