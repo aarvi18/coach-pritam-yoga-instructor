41b4d04c8c937254c1c38397a7a3b35a29402ed5
Rakesh <rakeshcse123@gmail.com>
2025-04-07 20:56:56 +0530

    refactor : reduce navbar padding

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC = () => {
 
     return (
         <nav className="w-full fixed top-0 left-0 bg-white shadow-md z-50">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2 flex items-center justify-between">
                 <NavbarLogo />
 
                 <div className="hidden md:flex flex-1 justify-center">
