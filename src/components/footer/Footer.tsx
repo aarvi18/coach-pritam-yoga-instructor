c0eb97ff6ec189656ee42adbbf2ead5f52b84a8b
Rakesh <rakeshcse123@gmail.com>
2025-04-10 20:40:05 +0530

    style : update footer ui

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import { FooterBottom } from './FooterBottom'
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-gray-100 text-gray-700 mt-20">
+        <footer className="bg-gray-100 text-gray-700">
             <div className="max-w-7xl mx-auto px-4 py-10 space-y-10">
                 <FooterTop />
                 <FooterLinks />
